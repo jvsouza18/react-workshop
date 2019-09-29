a0d1ccb3b007a5d9581f7346ba21a76d2bcaeafa
João Souza <joao.souza@modalgr.com.br>
2019-09-29 14:53:43 -0300

    subindo alterações

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { DetalhesPage } from "./pages/detalhes";
 import {HomePage } from "./pages/home";
 import {NotFoundPage} from "./pages/notFound";
-import {BrowserRouter,Route,Switch} from "react-router-dom";
+import {BrowserRouter,Route,Switch,Link} from "react-router-dom";
+import {AppBar, Toolbar, Typography, Button, Container } from '@material-ui/core';
 
 function App() {
+  
   return (
     <BrowserRouter>
-      <Switch>
-      <Route component={HomePage} exact path="/"></Route>
-      <Route component={DetalhesPage} path="/detalhes" ></Route>
-      <Route component={NotFoundPage}></Route>
-      </Switch>
+      <AppBar position="sticky" >
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
+            <Typography variant="h6">Home</Typography>
+          </Button>
+          <Button color="inherit" component={Link} to="/detalhes">
+            <Typography variant="h6">Detalhes</Typography>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Switch>
+          <Route key='/' component={HomePage} exact path="/"></Route>
+          <Route key='/detalhes' component={DetalhesPage} path="/detalhes" ></Route>
+          <Route key='N' component={NotFoundPage}></Route>
+        </Switch>
+        </Container>
     </BrowserRouter>
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import {AppBar, Toolbar, Typography, Card, Button, CardContent, CardMedia, Container } from '@material-ui/core';
 
-export function HomePage(){
-    return <h1>Home, Oi sou uma home page!</h1>
+
+const Digimon = (props) => (
+    <Card elevation={2} style={ { display: 'inline-block',  margin:'5px', width: '20%' } }>
+        <CardContent>
+            <CardMedia src={props.img} alt={`Imagem do digimon ${props.name}`} component="img" />
+            <Typography gutterBottom variant='h5' >{props.name}</Typography>
+            <Typography variant='body2'><p>Level: {props.level}</p></Typography>
+        </CardContent>
+    </Card>
+);
+
+
+export function HomePage()
+{ 
+    const [data, setData] = useState([])
+
+    useEffect(()=> {
+        fetch('https://digimon-api.herokuapp.com/api/digimon')
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+              console.log("Resultado da requisição:",response);
+              setData(response);
+        })
+        .catch((error)=>{
+          console.log(error);
+        });
+    }, [setData])
+   
+        if(data.length > 0)
+        {
+            return (
+                data.map((digimon)=> (
+                    <div>
+                        <Digimon key={digimon.name} {...digimon} />
+                    </div>
+                )
+            ));
+        }
+        else
+        {
+            return <p>Carregando....</p>
+        }
 }
